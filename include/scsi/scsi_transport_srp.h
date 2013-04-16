96fd5f34bf6097dd0ddace3158dc4de7cd6ce469
Sebastian Riemer <sebastian.riemer@profitbricks.com>
2013-04-16 15:59:45 +0200

    IB/srp: Show that a reconnect can work
    EXPERIMENTAL! TEMPORARY!
    
    We add the rport sysfs file 'reconnect' in order to manually
    reconnect the target. This only works if the SCSI devices
    are already offline.
    
    For this we set the target state to 'SRP_TARGET_LIVE' again
    and call srp_reconnect_target().
    
    Signed-off-by: Sebastian Riemer <sebastian.riemer@profitbricks.com>

--- include/scsi/scsi_transport_srp.h
+++ include/scsi/scsi_transport_srp.h
@@ -32,6 +32,7 @@ struct srp_function_template {
 	void (*rport_delete)(struct srp_rport *rport);
 	int (*rport_set_rq_tmo)(struct srp_rport *rport);
 	bool (*scmnd_queue_tl_err_work)(struct scsi_cmnd *scmnd);
+	int (*rport_reconnect)(struct srp_rport *rport);
 	/* for target drivers */
 	int (* tsk_mgmt_response)(struct Scsi_Host *, u64, u64, int);
 	int (* it_nexus_response)(struct Scsi_Host *, u64, int);
